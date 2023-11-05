05e7bb064fc2ed72167f7c0aaf3f605bbce9a3c9
Sheheryar Shah <48985068+SheheryarShahVozye@users.noreply.github.com>
2023-11-05 23:47:17 +0500

    updates

--- junxApp/junxApp/MVVM/Views/Tabbar/LocationScreens/LocationDetailScreen.swift
+++ junxApp/junxApp/MVVM/Views/Tabbar/LocationScreens/LocationDetailScreen.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import MapKit
 
 struct LocationDetailScreen: View {
+    @State private var selectedHour: Int?
     @State var geometry: GeometryProxy
     @State private var region = MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), // Replace with your desired coordinates
@@ -35,7 +36,7 @@ struct LocationDetailScreen: View {
                     
                     Spacer()
                 }.padding(.leading,30)
-                    .padding(.top,10)
+                    .padding(.top,30)
                 
                 HStack{
                     BorderView(title: .constant(""))
@@ -49,29 +50,92 @@ struct LocationDetailScreen: View {
                     .overlay(
                         Map(coordinateRegion: $region)
                         .frame(width: geometry.size.width - 45,height: geometry.size.height * 0.34)
-                        .clipShape(RoundedRectangle(cornerRadius: 35))
+                        .clipShape(RoundedRectangle(cornerRadius: 30))
+                        .overlay(
+                          RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 122,height: 34)
+                            .foregroundStyle(Color("onlyblack").opacity(0.4))
+                            .overlay(
+                                HStack{
+                                    Image("Share 1")
+                                        .resizable()
+                                        .frame(width: 23,height: 23)
+                                    Text("Share Location")
+                                        .font(.customFont(name: .inter, type: .semiBold, size: 10))
+                                        .foregroundStyle(Color("whiteonly"))
+                                }
+                            ).offset(x:80,y:100)
+                        )
                     )
-                    
-                HStack {
-                    Text("Hangout Time")
-                        .font(.customFont(name: .manuale, type: .semiBold, size: 24))
-                        .foregroundStyle(Color("text-main-2"))
-                    
-                    Spacer()
-                    
-                    Image("More Icon")
-                        .resizable()
-                        .frame(width: 24,height: 24)
+                Group{
+                    HStack {
+                        Text("Hangout Time")
+                            .font(.customFont(name: .manuale, type: .semiBold, size: 24))
+                            .foregroundStyle(Color("text-main-2"))
+                        
+                        Spacer()
+                        
+                        Image("More Icon")
+                            .resizable()
+                            .frame(width: 24,height: 24)
 
+                        
+                        
+                    }.padding(.horizontal,30)
+                        .padding(.top,10)
                     
-                    
-                }.padding(.horizontal,30)
-                    .padding(.top,10)
+                    VStack {
+                           
+
+                             HStack {
+                                 ForEach(1...5, id: \.self) { hour in
+                                     VStack{
+                                         Text("\(hour)")
+                                             .font(.customFont(name: .inter, type: .regular, size: 20))
+                                             .foregroundColor(selectedHour == hour ? Color("032E49") : Color("simpleblack"))
+                                             .opacity(opacity(for: hour))
+                                         Text("Hours")
+                                             .font(.customFont(name: .inter, type: .regular, size: 14))
+                                             .foregroundColor(selectedHour == hour ? Color("032E49") : Color("simpleblack"))
+                                             .opacity(opacity(for: hour))
+                                     }.padding(.horizontal,12)
+                                     .onTapGesture {
+                                         selectedHour = hour
+                                     }
+                                     
+                                 }
+                             }.padding()
+
+                            
+                         }
+                         
+                }.padding(.top,10)
+              
                 Spacer()
+              
+                CustomButton(title: "Continue", width: Constants.size.width - 100, action: {
+                    
+                }).padding(.bottom,20)
+                
+                
                 
             }
         }.frame(width: geometry.size.width)
     }
+    
+    func opacity(for hour: Int) -> Double {
+        if let selectedHour = selectedHour {
+               let difference = abs(selectedHour - hour)
+               if difference == 0 {
+                   return 1.0 // Selected hour
+               } else if difference == 1 {
+                   return 0.5 // Surrounding hours
+               } else {
+                   return 0.25 // Fully faded
+               }
+           }
+           return 1.0 // Default opacity
+       }
     
     func BorderView(title: Binding<String>) -> some View {
         ZStack {
