3b21608ad22c09bd60510d609f6f50dbd7cc3c21
Hamza Alam Hashmi <hamzahalam2001@gmail.com>
2023-11-09 23:12:59 +0500

    fixed explore view

--- junxApp/junxApp/MVVM/Views/Tabbar/Explore/ExploreMembersView.swift
+++ junxApp/junxApp/MVVM/Views/Tabbar/Explore/ExploreMembersView.swift
@@ -13,19 +13,77 @@ struct ExploreMembersView: View {
     
     @ObservedObject var homeVM: HomeViewModel
     
+    @State var searchedText = ""
+    @State var isSearching = false
+    
     var body: some View {
         VStack {
-            Text("Hello, World!")
+            
+            SearchBar(text: $searchedText, isSearching: $isSearching)
+                .padding(.horizontal, 40)
+            
+            Text("Let’s Explore Our Community!")
+                .font(.customFont(name: .manuale, type: .semiBold, size: 24))
+            
+            ScrollView {
+                LazyVGrid(columns: [.init(), .init(), .init()], content: {
+                    
+                    ForEach(0 ... 15, id: \.self) { id in
+                        let index = id % homeVM.users.count
+                        UserView(user: homeVM.users[index])
+                    }
+                })
+            }
         }
         .toolbar(content: {
             ToolbarItem(placement: .topBarLeading) {
                 Image(systemName: "arrow.left")
                     .onTapGesture(perform: dismiss.callAsFunction)
             }
+            
+            ToolbarItem(placement: .principal) {
+                Image("text+image logo")
+            }
         })
+        .toolbarTitleDisplayMode(.inline)
+    }
+    
+    func UserView(user: AppUser) -> some View {
+        
+        let width = Constants.size.width / 3
+        return ZStack {
+            
+            Image(user.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: width, height: width)
+                .scaledToFill()
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+            
+            VStack(alignment: .leading) {
+                
+                Spacer()
+                
+                Text(user.name)
+                    .font(.customFont(name: .manuale, type: .bold, size: 14))
+                
+                HStack {
+                    Image("eye")
+                    
+                    Text("4.7K Views")
+                        .font(.customFont(name: .inter, type: .regular, size: 12))
+                }
+                .padding(.bottom, 5)
+            }
+        }
+        .frame(width: width, height: width)
+        .foregroundStyle(.white)
+        .multilineTextAlignment(.leading)
     }
 }
 
 #Preview {
-    ExploreMembersView(homeVM: HomeViewModel())
+    NavigationView {
+        ExploreMembersView(homeVM: HomeViewModel())
+    }
 }
