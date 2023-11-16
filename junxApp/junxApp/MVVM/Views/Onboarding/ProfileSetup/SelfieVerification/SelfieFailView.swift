2316a1be38deb816f5e68205137413a3428bbdbd
Sheheryar Shah <48985068+SheheryarShahVozye@users.noreply.github.com>
2023-11-17 01:40:36 +0500

    updayrd

--- junxApp/junxApp/MVVM/Views/Onboarding/ProfileSetup/SelfieVerification/SelfieFailView.swift
+++ junxApp/junxApp/MVVM/Views/Onboarding/ProfileSetup/SelfieVerification/SelfieFailView.swift
@@ -9,81 +9,77 @@ import SwiftUI
 
 struct SelfieMatchView: View {
     @State var geometry: GeometryProxy
-    var body: some View {
-        ZStack{
-            WhiteBackground()
-            VStack{
-                Spacer()
-                VStack{
-                    Image("JunX text")
-                        .padding(.bottom,60)
-                   
-                    HStack {
-                        
-                        Text("Selfie Matched Succesfully")
-                            .font(.customFont(name: .inter, type: .medium, size: 14))
-                        .foregroundStyle(Color("424753"))
-                        .multilineTextAlignment(.center)
+       var body: some View {
+           ZStack{
+               WhiteBackground()
+               VStack{
+                   Spacer()
+                   VStack{
+                       Image("JunX text")
+                           .padding(.bottom,50)
                       
-                    }.padding(.horizontal,30)
-                       
-                }.offset(y:-90)
-               
+                       HStack {
+                           
+                           Text("Selfie Matched Succesfully")
+                               .font(.customFont(name: .inter, type: .medium, size: 14))
+                           .foregroundStyle(Color("424753"))
+                           .multilineTextAlignment(.center)
+                         
+                       }.padding(.horizontal,30)
+                          
+                   }.padding(.bottom,20)
+                  
+                  
                    
-                
-                
-                Spacer()
-                
-                CustomButton(title: "Continue", width: 295) {
-                    
-                }.padding(.bottom,20)
-               
-            }
-        }
-    }
+                   Spacer()
+                   
+                   CustomButton(title: "Continue", width: 295) {
+                       
+                   }.padding(.bottom,20)
+                  
+               }
+           }
+       }
 }
 
 
 struct SelfieFailView: View {
     @State var geometry: GeometryProxy
-    var body: some View {
-        ZStack{
-            WhiteBackground()
-            VStack{
-                Spacer()
-                
-                VStack{
-                    Image("JunX text")
-                        .padding(.bottom,60)
-                    HStack {
-                        
-                        Text("Verification ")
-                            .font(.customFont(name: .inter, type: .regular, size: 14))
-                        .foregroundStyle(Color("424753"))
-                        + Text("Failed!")
-                            .font(.customFont(name: .inter, type: .bold, size: 14))
-                            .foregroundStyle(Color("FF3D00"))
+       var body: some View {
+           ZStack{
+               WhiteBackground()
+               VStack{
+                   Spacer()
+                   Image("JunX text")
+                       .padding(.bottom,50)
+                  
+                   HStack {
+                       
+                       Text("Verification ")
+                           .font(.customFont(name: .inter, type: .regular, size: 14))
+                       .foregroundStyle(Color("424753"))
+                       + Text("Failed!")
+                           .font(.customFont(name: .inter, type: .bold, size: 14))
+                           .foregroundStyle(Color("FF3D00"))
 
-                      
-                    }.padding(.horizontal,60)
-                    
-                    Text("Please retake your selfie in good lighting.")
-                        .font(.customFont(name: .inter, type: .regular, size: 14))
-                        .foregroundStyle(Color("424753"))
-                }.offset(y:-90)
-               
+                     
+                   }.padding(.horizontal,30)
                    
-               
-                
-                Spacer()
-                
-                CustomButton(title: "Retake", width: 295) {
-                    
-                }.padding(.bottom,20)
-               
-            }
-        }
-    }
+                   Text("Please retake your selfie in good lighting.")
+                       .font(.customFont(name: .inter, type: .regular, size: 14))
+                       .foregroundStyle(Color("424753"))
+                      
+                 
+                   
+                   Spacer()
+                   
+                   CustomButton(title: "Retake", width: 295) {
+                       
+                   }.padding(.bottom,20)
+                  
+               }
+           }
+       }
 }
 
 #Preview {
--- junxApp/junxApp/MVVM/Views/Onboarding/ProfileSetup/SelfieVerification/VerifySelfieView.swift
+++ junxApp/junxApp/MVVM/Views/Onboarding/ProfileSetup/SelfieVerification/VerifySelfieView.swift
@@ -9,36 +9,33 @@ import SwiftUI
 
 struct VerifySelfieView: View {
     @State var geometry: GeometryProxy
-    var body: some View {
-        ZStack{
-            WhiteBackground()
-            VStack{
-                Spacer()
-                VStack{
-                    Image("JunX text")
-                        .padding(.bottom,60)
-                    
-                    HStack {
-                        
-                        Text("Please Wait, We're Verifying Your Identity.\n This May Take a Moment")
-                            .font(.customFont(name: .inter, type: .regular, size: 14))
-                            .foregroundStyle(Color("text-secondary-4"))
-                            .multilineTextAlignment(.center)
-                        
-                    }.padding(.horizontal,30)
-                        
-                }.offset(y:-90)
-               
-                Spacer()
-                
-                Image("Group 8757")
-                    .resizable()
-                    .frame(width: 51,height: 12)
-                    .padding(.bottom,70)
-               
-            }
-        }
-    }
+      var body: some View {
+          ZStack{
+              WhiteBackground()
+              VStack{
+                  Spacer()
+                  Image("JunX text")
+                      .padding(.bottom,30)
+                 
+                  HStack {
+                      
+                      Text("Please Wait, We're Verifying Your Identity.\n This May Take a Moment")
+                          .font(.customFont(name: .inter, type: .regular, size: 14))
+                      .foregroundStyle(Color("text-secondary-4"))
+                      .multilineTextAlignment(.center)
+                 
+                  }.padding(.horizontal,30)
+                      .padding(.vertical,30)
+                  Spacer()
+                  
+                  Image("Group 8757")
+                      .resizable()
+                      .frame(width: 51,height: 12)
+                      .padding(.bottom,40)
+                 
+              }
+          }
+      }
 }
 
 #Preview {
