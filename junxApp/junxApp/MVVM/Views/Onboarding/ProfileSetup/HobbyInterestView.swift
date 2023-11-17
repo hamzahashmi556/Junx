f47fefaf20109ac9ea1b8d883dc48655dc392838
Sheheryar Shah <48985068+SheheryarShahVozye@users.noreply.github.com>
2023-11-17 22:04:07 +0500

    assets

---
+++ junxApp/junxApp/MVVM/Views/Onboarding/ProfileSetup/HobbyInterestView.swift
@@ -0,0 +1,86 @@
+//
+//  HobbyInterestView.swift
+//  junxApp
+//
+//  Created by apple on 17/11/2023.
+//
+
+import SwiftUI
+
+struct HobbyInterestView: View {
+    @ObservedObject var onboardingVM: OnboardingViewModel
+    @State var geometry: GeometryProxy
+    @State var toolbarVisibility: Visibility
+    @State var email = ""
+    @State var wrongUser: Bool = true
+    var body: some View {
+        
+        
+        
+        ZStack {
+            
+            WhiteBackground()
+            
+            VStack {
+                
+                VStack(spacing: 18, content: {
+                    
+                    Text("What are Your Interests?")
+                        .padding(.top, 35)
+                        .font(.customFont(name: .manuale, type: .semiBold, size: 24))
+                        .foregroundStyle(.textMain)
+                    
+                  
+                       
+                    
+                })
+                .padding(.horizontal, 25)
+                
+                
+                
+                VStack(spacing: 15) {
+                    
+                    SelectionCard(title: "Woman", selected: .constant(false))
+                    SelectionCard(title: "Man", selected: .constant(true))
+                    SelectionCard(title: "Non-Binary", selected: .constant(false))
+                    SelectionCard(title: "Trans", selected: .constant(false))
+                    
+                   
+                    
+
+                }
+                .padding(.top, 35)
+                
+                Spacer()
+                
+                CustomButton(title: "Continue", width: geometry.size.width - 95) {
+                    
+                }
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar(toolbarVisibility, for: .navigationBar)
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Image("Back")
+            }
+            ToolbarItem(placement: .principal) {
+                Text("Profile Set Up")
+                    .font(.customFont(name: .SFPRO, type: .regular, size: 16))
+                
+            }
+            
+            
+            
+        })
+        
+    }
+}
+
+#Preview {
+    NavigationView {
+        GeometryReader { geo in
+            HobbyInterestView(onboardingVM: OnboardingViewModel(), geometry: geo, toolbarVisibility: .visible)
+        }
+    }
+}
